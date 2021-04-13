030e174b825db493efa1986d2cf1612d3eb4d490
John Champion <johnt.champion@gmail.com>
2021-04-12 20:48:23 -0400

    Added support for dark mode.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,20 +2,37 @@ import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 import cogImage from '../../assets/cogs.svg';
+import cogWhiteImage from '../../assets/cogs-white.svg';
 import closeImage from '../../assets/close.svg';
+import closeWhiteImage from '../../assets/close-white.svg';
 import classes from './Header.module.css';
 
 const Header = props => {
+
+    const settingsLink = (
+        <Link to="/settings">
+            <picture>
+                <source srcSet={cogWhiteImage} media="(prefers-color-scheme: dark)" />
+                <img src={cogImage} alt="Settings" />
+            </picture>
+        </Link>
+    );
+
+    const closeLink = (
+        <Link to="/">
+            <picture>
+                <source srcSet={closeWhiteImage} media="(prefers-color-scheme: dark)" />
+                <img src={closeImage} alt="Time Clock" />
+            </picture>
+        </Link>
+    );
+
     return (
         <Fragment>
             <header className={classes.Header}>
                 <div className={classes.headerName}>{props.name}</div>
                 <div className={classes.headerSettings}>
-                    {
-                        props.settings ?
-                            <Link to="/settings"><img src={cogImage} alt="Settings" /></Link> :
-                            <Link to="/"><img src={closeImage} alt="Time Clock" /></Link>
-                    }
+                    {props.settings ? settingsLink : closeLink}
                 </div>
             </header>
             <div className={classes.headerHeight}></div>
